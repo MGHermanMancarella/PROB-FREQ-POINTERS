cd4e4352f51d2c3db36a6d5057b7c78c5b2bbd99
Michael <mherman315@gmail.com>
2023-04-07 16:07:42 -0700

    working on avg pairs

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,21 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
-}
+'use strict';
 
+/** Checks sorted array for two numbers that average to targetAvg
+ * takes in a sorted array of numbers and a targetAvg
+ * Returns boolean
+ */
+function averagePair() {}
+
+/**averagePair»
+This should take a sorted array of numbers, nums, and a numeric targetAvg.
+Determine if there is a pair of numbers in nums where the average of the pair equals the target average.
+There may be more than one pair that matches the average target.
+
+Constraints: Time complexity: O(n)
+
+averagePair([1, 2, 3], 2.5);         // true
+averagePair([3, 3, 6, 12, 19], 8);   // false
+averagePair([1, 2, 3], 2);           // true
+averagePair([], 4);                  // false
+ */
+rparky5;
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,42 @@
-// add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+'use strict';
+
+/**
+ * checks to see if the word can be built using the letter argument's contents
+ * takes in two lowercase strings.
+ * returns boolean.
+ */
+function canConstructWord(word, letters) {
+  const letterCount = getFrequency(letters);
+
+  for (let char of word) {
+    if (!letterCount[char]) {
+      return false;
+    } else {
+      letterCount[char]--;
+      if (letterCount[char] < 0) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
+
+function getFrequency(letters) {
+  const freqCount = {};
+
+  for (let letter of letters) {
+    let currLetter = freqCount[letter] || 0;
+    freqCount[letter] = currLetter + 1;
+  }
+  return freqCount;
+}
+
+/** Write a frequency counter
+ * pass the letters into the counter
+ * return count obj
+ *
+ * loop the word and subtract 1 at each letter key in the counter
+ * if < 0 || undefined return false
+ * return true
+ *
+ */
